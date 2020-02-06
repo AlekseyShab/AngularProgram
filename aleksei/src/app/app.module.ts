51218dc0fd3bdac4cf44c035cfc4a0f6b0028538
Aleksei Shabunin <aleksei_shabunin@epam.com>
2020-02-06 15:29:58 +0300

    Angular
    - List

--- aleksei/src/app/app.module.ts
+++ aleksei/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SectionListComponent } from './components/section-list/section-list.component';
+import { SectionListItemComponent } from './components/section-list-item/section-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SectionListComponent,
+    SectionListItemComponent,
   ],
   imports: [
     BrowserModule,
