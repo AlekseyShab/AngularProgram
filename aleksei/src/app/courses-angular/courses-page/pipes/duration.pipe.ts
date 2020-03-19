009e4c3508a47d5c8e314dfd03aea475fb3f7bb0
Aleksei Shabunin <aleksei_shabunin@epam.com>
2020-03-19 21:14:43 +0300

    Add pipe Order

--- aleksei/src/app/courses-angular/courses-page/pipes/duration.pipe.ts
+++ aleksei/src/app/courses-angular/courses-page/pipes/duration.pipe.ts
@@ -8,7 +8,11 @@ export class DurationPipe implements PipeTransform {
   transform(value: number): string {
     const hours = Math.floor(value/60);
     const minutes: number = Math.floor(value % 60);
-    return `${hours} h, ${minutes} min`;
+    if(hours < 1 ){
+      return `${minutes} min`
+    }else{
+      return `${hours} h, ${minutes} min`;
+    }
   }
 
 }
