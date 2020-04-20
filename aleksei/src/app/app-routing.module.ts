41af49640e88a055292a2362e620c071837e2d65
Aleksei Shabunin <aleksei_shabunin@epam.com>
2020-04-20 14:10:29 +0300

    Update routing, add guards

--- aleksei/src/app/app-routing.module.ts
+++ aleksei/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CoursesPageComponent } from './courses-angular/courses-page/courses-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { CreateFileModalWindowComponent } from './courses-angular/courses-page/create-file-modal-window/create-file-modal-window.component';
+import { AuthGuard } from './guards/auth.guard';
+import { CoursesPageComponent } from './courses-angular/courses-page/courses-page.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
     {
       path: 'courses',
-      component: CoursesPageComponent,
+      canActivate: [AuthGuard],
+      canLoad: [AuthGuard],
+      component: CoursesPageComponent
     },
   {
     path: 'courses/new', component: CreateFileModalWindowComponent
@@ -22,6 +25,10 @@ const routes: Routes = [
   },
   {
     path: 'error-page',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
     component: ErrorPageComponent
   }
 ];
