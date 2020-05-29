0409564c6db5ff1f51a9aeba7b739c800a2adf01
Aleksei Shabunin <aleksei_shabunin@epam.com>
2020-05-29 19:36:30 +0300

    fix

--- aleksei/src/app/courses-angular/courses-page/list/list.component.ts
+++ aleksei/src/app/courses-angular/courses-page/list/list.component.ts
@@ -3,7 +3,7 @@ import { CourseItem } from '../models/typescript-course.model';
 import { CoursePageService } from '../services/course-page.service';
 import { DeleteFileModalWindowComponent } from '../delete-file-modal-window/delete-file-modal-window.component';
 import { MatDialog } from '@angular/material';
-import { Subscription } from 'rxjs';
+import { throwError } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 @Component({
@@ -21,7 +21,6 @@ export class ListComponent implements OnInit {
       private dialog: MatDialog,
       private store: Store<{list: CourseItem[]}>
   ) {}
-  deleteOperationSuccessfulSubscription: Subscription;
 
   ngOnInit() {
     this.store.dispatch({ type: '[List Page] Load Courses' });
@@ -31,44 +30,17 @@ export class ListComponent implements OnInit {
     },
                err => {
               this.showSpinner = false;
-              console.log(err)
+              throwError(err)
             }
         )
-    // this.courseService.getCoursesHttp().subscribe(
-    //     value => {
-    //
-    //   this.arrayOfCourses = value;
-    // },
-
   }
-
-  // ngOnChanges() {
-  //   this.deleteOperationSuccessfulSubscription = this.courseService.deleteOperationSuccessfulEvent$
-  //       .subscribe(isSuccessful => {
-  //         if (isSuccessful === true) {
-  //           this.courseService.getCoursesHttp().subscribe(value => {
-  //             this.arrayOfCourses = value;
-  //           });
-  //         } else {
-  //           this.showSpinner = false;
-  //           console.log('error')
-  //         }
-  //       });
-  // }
-
-  // ngOnDestroy() {
-  //   this.deleteOperationSuccessfulSubscription.unsubscribe();
-  // }
 
   openDialog(id): void {
     const dialogRef = this.dialog.open(DeleteFileModalWindowComponent, {
       width: '350px',
       data: id
     });
-
-    dialogRef.afterClosed().subscribe(() => {
-      console.log('The dialog was closed');
-    });
+    dialogRef.afterClosed()
   }
 
 }
